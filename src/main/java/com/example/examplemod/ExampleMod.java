575d07a9cf04d33c35e0ddd3e53b325a5bf19ed1
elfin8er <elfin8er@gmail.com>
2015-12-02 21:03:04 -0800

    TEST

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -7,7 +7,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod {
-    public static final String MODID = "asdf";
+    public static final String MODID = "Hello World";
     public static final String VERSION = "1.0";
     
     @EventHandler
